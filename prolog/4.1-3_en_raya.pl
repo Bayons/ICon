2dd08411594bde872956b39217b62a2a87012a9a
Miguel Bayón <mbayons@gmail.com>
2019-10-30 03:12:48 +0100

    Terminado 3 en raya, falta comentar
    Apoyado por el archivo de aulas.inf.uva.es

--- prolog/4.1-3_en_raya.pl
+++ prolog/4.1-3_en_raya.pl
@@ -23,3 +23,61 @@ linea(3,6,9).
 % Diagonales
 linea(1,5,9).
 linea(3,5,7).
+
+%%% REGLAS
+%% Amenaza
+%
+amenaza([X, Y, Z], B, X):-
+	vacio(X, B),
+	cruz(Y, B),
+	cruz(Z, B).
+
+%
+amenaza([X, Y, Z], B, Y):-
+       cruz(X, B),
+       vacio(Y, B),
+       cruz(Z, B).
+
+%
+amenaza([X, Y, Z], B, Z):-
+	cruz(X, B),
+	cruz(Y, B),
+	vacio(Z, B).
+
+%% Vacio
+%
+vacio(Casilla, Tablero):-
+	argumento(Casilla, Tablero, Valor),
+	Valor=[].
+
+%% Cruz
+%
+cruz(Casilla, Tablero):-
+	argumento(Casilla, Tablero, Valor),
+	Valor=x.
+
+%% Cara
+%
+cara(Casilla, Tablero):-
+	argumento(Casilla, Tablero, Valor),
+	Valor=o.
+
+%% Argumento
+%
+argumento(Posicion, Lista, X):-
+	Posicion=1,
+	arg(1, Lista, X).
+
+%
+argumento(Posicion, Lista, X):-
+	Posicion>1,
+	arg(2, Lista, Y),
+	Pos is Posicion-1,
+	argumento(Pos, Y, X).
+
+%% Movimiento forzoso
+%
+movimiento_forzoso(Tablero, Casilla) :-
+	linea(Linea),
+	amenaza(Linea, Tablero, Casilla),
+	!. 
