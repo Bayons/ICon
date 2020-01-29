b05ec1545b48d9cc96cedcff0761b7eeee47aee8
Miguel Bayón <mbayons@gmail.com>
2020-01-29 19:11:23 +0100

    Añadido meta-interprete a 5.4
    - Añadido meta-interprete Vanilla mejorado
    - Añadida explicacion al final

--- prolog/5.4_ejemplo_de_examen.pl
+++ prolog/5.4_ejemplo_de_examen.pl
@@ -25,3 +25,44 @@ comprueba(L, Res):-
 	Ef = eF,
 	Res is 1,
 	!.
+
+solve(A):-
+    predicate_property(A,built_in),
+    !,
+    call(A).
+solve(true):-!.
+solve((A,B)):-
+        !,
+        solve(A),
+        solve(B).
+solve(A):-
+        clause(A,B),
+        solve(B).
+
+
+% --------------------------
+%	EXPLICACION
+% --------------------------
+%
+% Este ejercicio es algo más complicado de hacer si se hace con una sola
+% pila. Los estados no deben depender de las pilas, por lo tanto no
+% resulta lógico plantear diferentes acciones con la misma entrada y en el
+% mismo estado. Sin embargo, con una sola pila (y siendo el número más
+% largo el de en medio) parece que esta es la única solución. Por tanto,
+% la explicación de los estados es:
+%
+% Estado 1.
+% 	Siempre que reciba una 'a', añadirá este símbolo a la pila.
+% 	Cambiará de estado al encontrar una 'b'.
+% Estado 2.
+% 	Siempre que encuentre una 'b', quitará una 'a' de la pila. Cuando
+% 	la pila se quede vacía (y si sigue encontrando el símbolo 'b'),
+% 	añadirá el símbolo a la pila.
+% Estado 3.
+% 	Siempre que encuentre una 'c', quitará una 'b' de la pila hasta
+% 	vaciarlo.
+%
+% El autómata finaliza correctamente si la lista y la pila están vacías.
+% De esta manera, nos aseguramos que el funcionamiento es correcto. Sin
+% embargo, cabe replantearse si este es el mejor comportamiento teniendo
+% en cuenta lo anterior.
